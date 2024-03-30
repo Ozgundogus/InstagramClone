e4c6c9a6e22ab7b7d9c369b29824ca353bdcb8de
Ozgundogus <ozgundogus.can@ogr.iuc.edu.tr>
2024-03-30 21:06:15 +0300

    first commit
    --> Setup programmatically UI

---
+++ InstagramClone/InstagramClone/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Ozgun Dogus on 30.03.2024.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+       
+    }
+
+
+}
+
